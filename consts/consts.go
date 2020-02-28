7a571557444d8f42dc5f7cd0cedeaf6b13180d0c
aper <lxsdwlhh@gmail.com>
2020-02-28 19:39:05 +0800

    change url

--- consts/consts.go
+++ consts/consts.go
@@ -1,17 +1,20 @@
 package consts
 
-//爬虫URL
+//Url 爬虫URL
 const (
-	COMMENT_URL_PREFIX = "https://itunes.apple.com/WebObjects/MZStore.woa/wa/userReviewsRow?cc=cn&displayable-kind=11&sort=0&appVersion=all"
-	VERSION_URL_PREFIX = "https://itunes.apple.com/rss/customerreviews/"
-	VERSION_URL_SUFFIX = "/sortby=mostrecent/json?l=en&&cc=cn"
+	CommentUrlPrefix = "https://itunes.apple.com/WebObjects/MZStore.woa/wa/userReviewsRow?cc=cn&displayable-kind=11&sort=0&appVersion=all"
+	VersionUrlPrefix = "https://itunes.apple.com/rss/customerreviews/"
+	VersionUrlSuffix = "/sortby=mostrecent/json?l=en&&cc=cn"
 )
 
-//时间格式化模板
-const TIME_STR = "2006-01-02 15:04:05"
+//TimeStr 时间格式化模板
+const TimeStr = "2006-01-02 15:04:05"
 
 //Status 数据状态
 const (
-	NORMAL = 1 //启用中
-	UNUSED = 2 //未启用
+	Normal = 1 //启用中
+	Unused = 2 //未启用
 )
+
+//PageSize 爬虫偏移量
+const PageSize = 100
